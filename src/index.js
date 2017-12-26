bce9d78cc4d9e0ebf3f08ef9c169a2e78cfc6ce8
Anton Ignatov <abietis@gmail.com>
2017-12-26 10:47:59 +0300

    Updated build config

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import * as TVDML from 'tvdml';
 
 TVDML
   .subscribe(TVDML.event.LAUNCH)
-  .pipe(TVDML.render(
+  .pipe(TVDML.render(() => (
     <document>
       <alertTemplate>
         <title>Hello world</title>
       </alertTemplate>
     </document>
-  ));
+  )));
